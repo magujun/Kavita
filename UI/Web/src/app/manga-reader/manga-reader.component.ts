afc9ee249a840ec1d50e5fc6f7854b337ae76b97
marcelo <guimaraesjunior.marcelo@gmail.com>
2022-08-16 23:25:46 -0700

    Swipe handlePageChange fix
    - Updated handlePageChange() with strong typed direction (right/left/up/down)

--- UI/Web/src/app/manga-reader/manga-reader.component.ts
+++ UI/Web/src/app/manga-reader/manga-reader.component.ts
@@ -972,7 +972,7 @@ export class MangaReaderComponent implements OnInit, AfterViewInit, OnDestroy {
     }
   }
 
-  handlePageChange(event: any, direction: string) {
+  handlePageChange(event: any, direction: 'right' | 'left' | 'up' | 'down') {
     if (this.readerMode === ReaderMode.Webtoon) {
       if (direction === 'right') {
         this.nextPage(event);
@@ -981,9 +981,9 @@ export class MangaReaderComponent implements OnInit, AfterViewInit, OnDestroy {
       }
       return;
     }
-    if (direction === 'right') {
+    if (direction === 'right' || direction === 'down') {
       this.readingDirection === ReadingDirection.LeftToRight ? this.nextPage(event) : this.prevPage(event);
-    } else if (direction === 'left') {
+    } else if (direction === 'left' || direction === 'up') {
       this.readingDirection === ReadingDirection.LeftToRight ? this.prevPage(event) : this.nextPage(event);
     }
   }
